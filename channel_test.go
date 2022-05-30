19443e79d24cdea2cfc96e089d013fd5e1b4fc70
rizkitirta <rizkibuwono0@gmail.com>
2022-05-30 09:59:34 +0700

    Buffered channel

--- channel_test.go
+++ channel_test.go
@@ -60,3 +60,24 @@ func TestInOutChannel(t *testing.T) {
 	time.Sleep(2 * time.Second)
 	println("channel selesai")
 }
+
+// buffer channel atau limit memasukan data ke channel
+func TestBufferChannel(t *testing.T) {
+	channel := make(chan string, 2) // 2 adalah limit 
+	defer close(channel)
+
+	go func() {
+		channel <- "data 1"
+		channel <- "data 2"
+	}()
+	go func() {
+		// data diambil sesuai urutan saat memasukan data ke channel
+		data1 := <-channel
+		data2 := <-channel
+		println(data1)
+		println(data2)
+	}()
+
+	time.Sleep(2 * time.Second)
+	println("channel selesai")
+}
