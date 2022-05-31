a83f032b76ffd22fa7776ff7bf828523f44d1bf3
rizkitirta <rizkibuwono0@gmail.com>
2022-05-31 09:26:23 +0700

    Sync Once (menjalankan fungsi goroutine hanya satu kali

--- wait_group_test.go
+++ wait_group_test.go
@@ -6,20 +6,42 @@ import (
 	"time"
 )
 
-func RunAsync(group *sync.WaitGroup,number int) {
+func RunAsync(group *sync.WaitGroup, number int) {
 	defer group.Done()
 	group.Add(1)
-	
+
 	time.Sleep(1 * time.Second)
-	println("Perulangan ke-",number)
+	println("Perulangan ke-", number)
 }
 
 func TestWaitGroup(t *testing.T) {
 	group := sync.WaitGroup{}
 	for i := 0; i < 100; i++ {
-		go RunAsync(&group,i)
+		go RunAsync(&group, i)
 	}
 
 	group.Wait()
 	println("Wait Group Done")
+}
+
+var counter int = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	once := sync.Once{}
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			group.Add(1)
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	println("Counter:", counter)
 }
